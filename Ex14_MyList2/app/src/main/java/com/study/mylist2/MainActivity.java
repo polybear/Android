a0abc23706c602c5222c5f94b57a2062a11da092
Polybear <hmango01@naver.com>
2017-05-29 11:17:41 +0900

    예제 14 list2

---
+++ Ex14_MyList2/app/src/main/java/com/study/mylist2/MainActivity.java
@@ -0,0 +1,72 @@
+package com.study.mylist2;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    String[] names = { "홍길동", "강감찬", "을지문덕", "양만춘", "유관순" };
+    String[] ages = { "20", "25", "30", "35", "15" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+
+        final MyAdapter adapter = new MyAdapter();
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                Toast.makeText(getApplicationContext(), "selected : " + names[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() { return names.length; }
+
+        @Override
+        public Object getItem(int position) { return names[position]; }
+
+        @Override
+        public long getItemId(int position) { return position; }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LinearLayout layout = new LinearLayout(getApplicationContext());
+            layout.setOrientation(LinearLayout.VERTICAL);
+
+            TextView view1 = new TextView(getApplicationContext());
+            view1.setText(names[position]);
+            view1.setTextSize(40.0f);
+            view1.setTextColor(Color.BLUE);
+
+            layout.addView(view1);
+
+            TextView view2 = new TextView(getApplicationContext());
+            view2.setText(ages[position]);
+            view2.setTextSize(40.0f);
+            view2.setTextColor(Color.RED);
+
+            layout.addView(view2);
+
+            return layout;
+        }
+    }
+}
